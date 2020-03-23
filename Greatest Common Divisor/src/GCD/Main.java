21abaf9579a2076eba5385e33b474424a90db600
ArtemIlin <artemlex@gmail.com>
2020-03-23 12:14:55 +0300

    create method GCD

--- Greatest Common Divisor/src/GCD/Main.java
+++ Greatest Common Divisor/src/GCD/Main.java
@@ -4,4 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
     }
+    // Create method!
+    public static int getGCD(int numberOne, int numberTwo){
+        if (numberOne == 0) return numberTwo;
+        if (numberTwo == 0) return numberOne;
+        if (numberOne >= numberTwo) return getGCD(numberOne % numberTwo, numberTwo);
+        return getGCD(numberOne, numberTwo % numberOne);
+    }
 }
