8f01e9fd296699bcecfdcc6943734a2372e59528
ArtemIlin <artemlex@gmail.com>
2020-05-04 22:05:25 +0300

    Greatest Common Divisor added project

--- algorithms/src/GCD/GCD.java
+++ algorithms/src/GCD/GCD.java
@@ -1,4 +1,11 @@
 package GCD;
 
 public class GCD {
+    // Create method!
+    public static int getGCD(int numberOne, int numberTwo) {
+        if (numberOne == 0) return numberTwo;
+        if (numberTwo == 0) return numberOne;
+        if (numberOne >= numberTwo) return getGCD(numberOne % numberTwo, numberTwo);
+        return getGCD(numberOne, numberTwo % numberOne);
+    }
 }
--- algorithms/src/GCD/Main.java
+++ algorithms/src/GCD/Main.java
@@ -1,4 +1,20 @@
 package GCD;
 
+import java.util.Scanner;
+
+import static GCD.GCD.getGCD;
+
 public class Main {
+    public static void main(String[] args) {
+
+        // Test method
+        Scanner scanner = new Scanner(System.in);
+        int numberOne = scanner.nextInt();
+        int numberTwo = scanner.nextInt();
+        // Check compilation time
+        long start = System.currentTimeMillis();
+        System.out.println(getGCD(numberOne, numberTwo));
+        long end = System.currentTimeMillis();
+        System.out.println(end - start + " ms");
+    }
 }
